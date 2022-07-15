abab3fc3f4a601e07603698587d3e41308f3f1b1
chafik49 <chafik.raja@gmail.com>
2022-07-15 18:14:18 +0100

    fixing the config problem

--- next.config.js
+++ next.config.js
@@ -1,6 +1,8 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
+module.exports = {
+  env : {
+    mongodb_username : 'chafik',
+    mongodb_password : 'password2000',
+    mongodb_clustername : 'apple-shop-nextjs-clust',
+    mongodb_database : 'Apple-Shop'
+  }
 }
-
-module.exports = nextConfig
